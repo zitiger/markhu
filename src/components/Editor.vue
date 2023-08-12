69181468132841f7c4570f36cb1a47c9ca2bc00e
zitiger <zitiger@163.com>
2023-08-12 15:06:30 +0800

    feat: change toolbar tips position

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -163,7 +163,7 @@ const save = {
 
 const toolbar = [
   save,
-  "emoji",
+  /*"emoji",
   "headings",
   "bold",
   "italic",
@@ -190,7 +190,213 @@ const toolbar = [
   "redo",
   "|",
   "fullscreen",
-  "edit-mode",
+  "edit-mode",*/
+
+
+
+  {
+    hotkey: "⌘E",
+    icon: '<svg><use xlink:href="#vditor-icon-emoji"></use></svg>',
+    name: "emoji",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘H",
+    icon:
+        '<svg><use xlink:href="#vditor-icon-headings"></use></svg>',
+    name: "headings",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘B",
+    icon: '<svg><use xlink:href="#vditor-icon-bold"></use></svg>',
+    name: "bold",
+    prefix: "**",
+    suffix: "**",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘I",
+    icon: '<svg><use xlink:href="#vditor-icon-italic"></use></svg>',
+    name: "italic",
+    prefix: "*",
+    suffix: "*",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘D",
+    icon: '<svg><use xlink:href="#vditor-icon-strike"></use></svg>',
+    name: "strike",
+    prefix: "~~",
+    suffix: "~~",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘K",
+    icon: '<svg><use xlink:href="#vditor-icon-link"></use></svg>',
+    name: "link",
+    prefix: "[",
+    suffix: "](https://)",
+    tipPosition: "s",
+  },
+  {
+    name: "|",
+  },
+  {
+    hotkey: "⌘L",
+    icon: '<svg><use xlink:href="#vditor-icon-list"></use></svg>',
+    name: "list",
+    prefix: "* ",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘O",
+    icon:
+        '<svg><use xlink:href="#vditor-icon-ordered-list"></use></svg>',
+    name: "ordered-list",
+    prefix: "1. ",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘J",
+    icon: '<svg><use xlink:href="#vditor-icon-check"></use></svg>',
+    name: "check",
+    prefix: "* [ ] ",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⇧⌘I",
+    icon:
+        '<svg><use xlink:href="#vditor-icon-outdent"></use></svg>',
+    name: "outdent",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⇧⌘O",
+    icon: '<svg><use xlink:href="#vditor-icon-indent"></use></svg>',
+    name: "indent",
+    tipPosition: "s",
+  },
+  {
+    name: "|",
+  },
+  {
+    hotkey: "⌘;",
+    icon: '<svg><use xlink:href="#vditor-icon-quote"></use></svg>',
+    name: "quote",
+    prefix: "> ",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⇧⌘H",
+    icon: '<svg><use xlink:href="#vditor-icon-line"></use></svg>',
+    name: "line",
+    prefix: "---",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘U",
+    icon: '<svg><use xlink:href="#vditor-icon-code"></use></svg>',
+    name: "code",
+    prefix: "```",
+    suffix: "\n```",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘G",
+    icon:
+        '<svg><use xlink:href="#vditor-icon-inline-code"></use></svg>',
+    name: "inline-code",
+    prefix: "`",
+    suffix: "`",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⇧⌘B",
+    icon: '<svg><use xlink:href="#vditor-icon-before"></use></svg>',
+    name: "insert-before",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⇧⌘E",
+    icon: '<svg><use xlink:href="#vditor-icon-after"></use></svg>',
+    name: "insert-after",
+    tipPosition: "s",
+  },
+  {
+    name: "|",
+  },
+  {
+    icon: '<svg><use xlink:href="#vditor-icon-upload"></use></svg>',
+    name: "upload",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘M",
+    icon: '<svg><use xlink:href="#vditor-icon-table"></use></svg>',
+    name: "table",
+    prefix: "| col1",
+    suffix:
+        " | col2 | col3 |\n| --- | --- | --- |\n|  |  |  |\n|  |  |  |",
+    tipPosition: "s",
+  },
+  {
+    name: "|",
+  },
+  {
+    hotkey: "⌘Z",
+    icon: '<svg><use xlink:href="#vditor-icon-undo"></use></svg>',
+    name: "undo",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘Y",
+    icon: '<svg><use xlink:href="#vditor-icon-redo"></use></svg>',
+    name: "redo",
+    tipPosition: "s",
+  },
+  {
+    name: "|",
+  },
+
+  {
+    hotkey: "⌘'",
+    icon:
+        '<svg><use xlink:href="#vditor-icon-fullscreen"></use></svg>',
+    name: "fullscreen",
+    tipPosition: "s",
+  },
+  {
+    icon: '<svg><use xlink:href="#vditor-icon-edit"></use></svg>',
+    name: "edit-mode",
+    tipPosition: "s",
+  },
+  {
+    hotkey: "⌘P",
+    icon: '<svg><use xlink:href="#vditor-icon-both"></use></svg>',
+    name: "both",
+    tipPosition: "s",
+  },
+  {
+    icon:
+        '<svg><use xlink:href="#vditor-icon-preview"></use></svg>',
+    name: "preview",
+    tipPosition: "s",
+  },
+  {
+    icon: '<svg><use xlink:href="#vditor-icon-theme"></use></svg>',
+    name: "content-theme",
+    tipPosition: "s",
+  },
+  {
+    icon:
+        '<svg><use xlink:href="#vditor-icon-code-theme"></use></svg>',
+    name: "code-theme",
+    tipPosition: "s",
+  },
+  {
+    name: "br",
+  },
 ];
 
 const emoji = {
