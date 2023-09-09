4c217d396090e9696ea5c0e878d82f102f7981df
zitiger <zitiger@163.com>
2023-09-09 20:46:20 +0800

    chore: add version bump script

---
+++ version.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# 修改版本号
+# 运行示例: sh version.sh 0.0.1
+
+# update package.json version
+function update_package_version() {
+   local version=$1
+   local file_path='package.json'
+   local file_content=$(cat $file_path)
+   local new_file_content=$(echo "$file_content" | sed -E "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"$version\"/")
+   echo "$new_file_content" > $file_path
+}
+
+# update tauri.conf.json version
+function update_tauri_conf_version() {
+   local version=$1
+   local file_path='src-tauri/tauri.conf.json'
+   local file_content=$(cat $file_path)
+   local new_file_content=$(echo "$file_content" | sed -E "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/\"version\": \"$version\"/")
+   echo "$new_file_content" > $file_path
+}
+
+# update cargo.toml version
+function update_cargo_version() {
+   local version=$1
+   local file_path='src-tauri/Cargo.toml'
+   local file_content=$(cat $file_path)
+   local new_file_content=$(echo "$file_content" | sed -E "s/version = \"[0-9]+\.[0-9]+\.[0-9]+\"/version = \"$version\"/")
+   echo "$new_file_content" > $file_path
+}
+
+# 执行
+update_package_version $1
+update_tauri_conf_version $1
+update_cargo_version $1
